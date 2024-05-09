9014c557e0f3dcd2e690e961178ae0b307e3a349
ari130123 <aespinosa894@gmail.com>
2024-05-09 18:02:09 +0200

    añado paquete y clase con mi nombre

---
+++ Repo1Actividad1/src/Arantza/Arantza.java
@@ -0,0 +1,14 @@
+package Arantza;
+
+public class Arantza {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String nombre = "Arantza";
+		
+		System.out.println("hola soy  " + nombre);
+
+	}
+
+}
